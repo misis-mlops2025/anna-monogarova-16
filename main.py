5aac3d9cce03ab22c91a34e19642bdc113800877
monogarovaa <anna.monogarovaa@yandex.ru>
2025-12-06 12:45:48 +0300

    Fix develop version of sum_n function

--- main.py
+++ main.py
@@ -8,15 +8,6 @@ def main():
 
 
 if __name__ == "__main__":
-    def sum_n(*args):
-    return sum(args)
-
-
-def main():
-    numbers = [1, 2, 3, 4]
-    print(sum_n(*numbers))
-
-
-if __name__ == "__main__":
     main()
+
 
