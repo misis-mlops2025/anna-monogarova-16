130c6730c86882f9f0a2527c0817a456bb023060
monogarovaa <anna.monogarovaa@yandex.ru>
2025-12-06 12:40:48 +0300

    Change function to sum of N numbers using *args

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-def sum_three(a, b, c):
-    return a + b + c
+def sum_n(*args):
+    return sum(args)
 
 
 def main():
-    x = 2
-    y = 3
-    z = 4
-    print(sum_three(x, y, z))
+    numbers = [1, 2, 3, 4]
+    print(sum_n(*numbers))
+
+
+if __name__ == "__main__":
+    def sum_n(*args):
+    return sum(args)
+
+
+def main():
+    numbers = [1, 2, 3, 4]
+    print(sum_n(*numbers))
 
 
 if __name__ == "__main__":
